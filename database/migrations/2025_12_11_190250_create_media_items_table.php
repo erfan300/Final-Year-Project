d1d1503f6d4ffeedcc58644fd257db2ba0e79968
Erfan Soltani <220117609@aston.ac.uk>
2025-12-11 19:09:44 +0000

    Initial DB models
    Initial start on the database tables and models, will need to confirm that this is in compliance with the objectives before running php artisan migrate.

---
+++ database/migrations/2025_12_11_190250_create_media_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('media_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->string('event_name')->nullable();
+            $table->date('event_date')->nullable();
+            $table->string('file_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media_items');
+    }
+};
